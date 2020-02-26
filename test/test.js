f7ee34175e5045acdd94adbb07d1166caca78f8e
zzl <646172505@qq.com>
2020-02-26 21:20:26 +0800

    添加注释,添加任务队列线程 2.1.4版本

--- test/test.js
+++ test/test.js
@@ -1,26 +1,63 @@
  
- class testc
+ const zwthread = require('../lib/zwthread');
+
+ const signalobj = zwthread.ZWSignalObject;
+ const QPS      = zwthread.ZWQPSCtr;
+ const JobThread =zwthread.ZWJobArrayThread;
+
+ let obj = null;
+
+ let ff =  async function()
  {
-     
-     Prop1 = 1;
-
-     getinfo()
-     {
-         return '222';
-     }
-     toJSON()
-     {
-         return '11';
-     }
+    obj = new signalobj(-1);
+    console.log('wait...');
+    let getnoif = await obj.wait(1000);
+    console.log( 'wait obj',getnoif );
  }
+/*
+ ff();
+ setTimeout( ()=>{
 
- class AA extends testc
+    obj.notify({a:1}).then( (r)=>{
+        console.log('notif:',r);
+    } )
+
+ },2000);*/
+
+
+ let testqps = new QPS(1);
+
+ let f2 = async function()
  {
-     Prop2 = '';
+    while(1)
+    {
+        await testqps.checkQPS();
+        console.log('do job at:',new Date() );
+    }
  }
- let x = new AA();
- let obj = {};
+ //f2();
 
- Object.assign(obj,x);
- console.log( obj );
+ let threafunc = async function( in_job )
+ {
+     let out_job = {};
+     console.log('in_job:',in_job.a);
+     out_job.a = in_job.a + 1;
+     console.log('out_job:',out_job.a);
+     return Promise.resolve( out_job );
+ }
+ let testarr = new JobThread(threafunc);
+ testarr.start();
  
+ testarr.addOneJob( { a:2 } ).then( (r)=>{
+    console.log('addjob ok?:',r);
+ });
+
+ testarr.addOneJobAndWait( {a:3} ).then( (jobrelsout)=>{
+     console.log('job wait:',jobrelsout);
+ } )
+
+
+
+
+
+
